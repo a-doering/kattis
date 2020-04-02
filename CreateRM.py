56fcdab1fe6cda6e2b163c8841c0f427ab28568e
Andreas <35858164+a-doering@users.noreply.github.com>
2020-04-02 23:53:02 +0200

    Add file to create readme

---
+++ CreateRM.py
@@ -0,0 +1,33 @@
+class CreateRM():
+
+    # Initializes the class
+    def __init__(self, profile_url):
+        pass
+
+    # Find the locally saved solutions
+    def find_solutions():
+        pass
+    
+    # Check if name of problem exists
+    def check_name():
+        pass
+    
+    # Check difficulty of problem
+    def check_difficulty():
+        pass
+    
+    # Create statistics
+    def stats():
+        pass
+    
+    # Create profile statistics
+    def profile_stats(profile_url):
+        pass
+        
+    # Create the readme in markdown
+    def create():
+        pass
+
+
+if __name__ == "__main__":
+    pass
