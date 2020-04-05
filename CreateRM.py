25873acedc4cdb6424664a1baee55e71ab03ca49
Andreas <35858164+a-doering@users.noreply.github.com>
2020-04-05 20:31:07 +0200

    Add find_solutions in local dir

--- CreateRM.py
+++ CreateRM.py
@@ -1,17 +1,32 @@
+import os
+import requests
+
 class CreateRM():
 
     # Initializes the class
     def __init__(self, profile_url):
         pass
 
-    # Find the locally saved solutions
-    def find_solutions():
-        pass
+    def find_solutions(dirs, verbose=False):
+        '''Find all solution files in local directories, returns list of paths'''
+        solutions = []
+        for dir in dirs:
+            file_names = os.listdir(dir)
+            for file_name in file_names:
+                full_path = os.path.join(dir, file_name)
+                solutions.append(full_path)
+
+                if verbose:
+                    print('file path: ', full_path)
+
+        return solutions
+        
     
-    # Check if name of problem exists
-    def check_name():
-        pass
-    
+    def problem_exists(url):
+        '''Check if name of problem exists on kattis'''
+        return requests.get(url).status_code == 200
+
+
     # Check difficulty of problem
     def check_difficulty():
         pass
@@ -30,4 +45,13 @@ class CreateRM():
 
 
 if __name__ == "__main__":
-    pass
+    cwd = os.getcwd()
+    dirs = ['C++']
+    dirs[:] = [os.path.join(cwd, dir) for dir in dirs]
+    solutions = CreateRM.find_solutions(dirs, verbose=False)
+
+    for i in solutions:
+        print(i)
+
+    print(CreateRM.problem_exists("https://open.kattis.com/"))
+
