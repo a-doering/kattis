fe8e004f76e548cb455cedec4cdd5085c0085b9f
Andreas <35858164+a-doering@users.noreply.github.com>
2020-06-08 00:16:20 +0200

    Add 1 solution

---
+++ C++/railroad.cpp
@@ -0,0 +1,11 @@
+//kattis railroad
+#include <iostream>
+using namespace std;
+
+int main(){
+  int x,y;
+  cin>>x>>y;
+  if((4*x+3*y)%2==0)cout<<"possible"<<endl;
+  else cout<<"impossible"<<endl;
+  return 0;
+}
